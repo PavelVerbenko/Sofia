d0550bd6cd904fd528880361cd03c257a1dcf22e
username <ya.pasha.verbenko@yandex.ru>
2025-02-20 21:02:27 +0300

    Initial commit

---
+++ src/test/java/com/example/Sofia/SofiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sofia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
